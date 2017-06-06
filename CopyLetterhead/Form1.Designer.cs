36ef2712b2af56b767bb59c27e9af4a8b755df60
john.wine <john.wine@KS11LP519887>
2017-06-06 10:29:49 -0500

    Initial commit of file to GitHub

---
+++ CopyLetterhead/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CopyLetterhead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbWFX32 = new System.Windows.Forms.Label();
+            this.chkOfficeGroup = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkOverwrite = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Letterhead";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbWFX32
+            // 
+            this.lbWFX32.AutoSize = true;
+            this.lbWFX32.Location = new System.Drawing.Point(12, 29);
+            this.lbWFX32.Name = "lbWFX32";
+            this.lbWFX32.Size = new System.Drawing.Size(35, 13);
+            this.lbWFX32.TabIndex = 1;
+            this.lbWFX32.Text = "label1";
+            // 
+            // chkOfficeGroup
+            // 
+            this.chkOfficeGroup.AutoSize = true;
+            this.chkOfficeGroup.Checked = true;
+            this.chkOfficeGroup.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkOfficeGroup.Location = new System.Drawing.Point(12, 96);
+            this.chkOfficeGroup.Name = "chkOfficeGroup";
+            this.chkOfficeGroup.Size = new System.Drawing.Size(124, 17);
+            this.chkOfficeGroup.TabIndex = 2;
+            this.chkOfficeGroup.Text = "Update Office Group";
+            this.chkOfficeGroup.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WFX32 Location : ";
+            // 
+            // chkOverwrite
+            // 
+            this.chkOverwrite.AutoSize = true;
+            this.chkOverwrite.Checked = true;
+            this.chkOverwrite.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkOverwrite.Location = new System.Drawing.Point(12, 119);
+            this.chkOverwrite.Name = "chkOverwrite";
+            this.chkOverwrite.Size = new System.Drawing.Size(134, 17);
+            this.chkOverwrite.TabIndex = 4;
+            this.chkOverwrite.Text = "Overwrite Existing Files";
+            this.chkOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 145);
+            this.Controls.Add(this.chkOverwrite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkOfficeGroup);
+            this.Controls.Add(this.lbWFX32);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Set Letterhead";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbWFX32;
+        private System.Windows.Forms.CheckBox chkOfficeGroup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkOverwrite;
+    }
+}
+
---
+++ CopyLetterhead/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace CopyLetterhead
+{
+    public partial class Form1 : Form
+    {
+        string sWFX32;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sWFX32 = GetWFX32();
+            lbWFX32.Text = sWFX32;
+        }
+
+        //Get WFX32 location
+        private string GetWFX32()
+        {
+            string strWFX32;
+            strWFX32 = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\CCHWinFx", "NetIniLocation", "none").ToString();
+            if (strWFX32 == "none")
+                strWFX32 = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\CCHWinFx", "Dir", "none").ToString();
+            strWFX32 = strWFX32.TrimEnd('\0');
+            if (strWFX32 == "none")
+            {
+                MessageBox.Show("Please Run Workstation Setup before running this program");
+                Application.Exit();
+            }
+            return strWFX32;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //File Dialog to select file
+            OpenFileDialog BrowseFile = new OpenFileDialog();
+            string sFile;
+            
+            BrowseFile.Filter = "Letterhead Form (*.HP)|*.hp";
+            BrowseFile.RestoreDirectory = true;
+            if (BrowseFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Filter out Invalid files
+                    if ((sFile = BrowseFile.FileName) != null)
+                    {
+                        sFile = sFile.ToUpper();
+                        if (sFile.EndsWith(".HP"))
+                        {
+                            sFile = sFile.Substring(sFile.LastIndexOf("\\") + 1);
+                            if (!sFile.StartsWith("LW"))
+                            {
+                                MessageBox.Show("Selected file is not a valid Letterhead file");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Selected file is not a Laser Form file");
+                            return;
+                        }
+                    }
+                    CopyLetterhead(BrowseFile.FileName);
+                    MessageBox.Show("Letterhead Setup Completed");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error opening file \r\n " + ex.Message);
+                }
+            }
+        }
+
+        private void CopyLetterhead(string File)
+        {
+            CopyLaserForm(File);
+            //Create a TRANSMIT.xml file referencing updated letterhead number
+            if (chkOfficeGroup.Checked)
+            {
+                string FormNumber = File.Substring(File.LastIndexOf("LW") + 2, 4);
+                string OfficeGR = sWFX32 + "\\OFFICEGR";
+                string Working;
+                List<string> Groups = new List<string>();
+                foreach (string d in Directory.GetDirectories(OfficeGR))
+                {
+                    Working = d.Substring(d.LastIndexOf('\\') + 1);
+                    Groups.Add(Working);
+                }
+                //If no directories were found in OFFICEGR something has gone wrong, and exit
+                if (Groups.Count == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    //If only 1 Office Group folder, assume correct
+                    if (Groups.Count == 1)
+                    {
+                        Working = Groups[0];
+                        Working = OfficeGR + "\\" + Working + "\\TRANSMIT.xml";
+                        EditTransmitXML(Working, FormNumber);
+                    }
+                    else
+                    {
+                        //Create Dialog prompt to select which office groups to update
+                        List<int> Selected = Prompt.GetItem(Groups);
+                        if (Selected[0] == -1)
+                            return;
+                        else
+                        {
+                            foreach (int i in Selected)
+                            {
+                                Working = Groups[i];
+                                Working = OfficeGR + "\\" + Working + "\\TRANSMIT.xml";
+                                EditTransmitXML(Working, FormNumber);
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+
+
+        //Get all subdirectories of WFX32, and if Subdirectory contains a LASER folder, as only Product folders should, copy LW file
+        private void CopyLaserForm(string FilePath)
+        {
+            string Destination = FilePath.Substring(FilePath.LastIndexOf('\\'));
+            foreach (string d in Directory.GetDirectories(sWFX32))
+            {
+                foreach (string s in Directory.GetDirectories(d))
+                    if (s.EndsWith("\\LASER"))
+                    {
+                        try
+                        {
+                            File.Copy(FilePath, s + Destination, chkOverwrite.Checked);
+                        }
+                        catch (IOException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }                        
+                    }
+            }
+        }
+
+        private void EditTransmitXML(string FilePath, string FormNum)
+        {
+            try
+            {
+                //We are only editing one value of the XML file, no need for a full XML parse
+                //Read file into output string
+                string output;
+                if (File.Exists(FilePath))
+                {
+                    FileStream file = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+                    using (StreamReader reader = new StreamReader(file, Encoding.ASCII))
+                    {
+                        output = reader.ReadToEnd();
+                        reader.Close();
+                        file.Close();
+                    }
+                }
+                else
+                {
+                    //Current default value of a brand new TRANSMIT.XML, possibly throw error instead
+                    output = "<officeGroupOptions><transmit ogid=\"000\"><headLine1></headLine1><headLine2></headLine2><headLine3></headLine3><headLine4></headLine4><close></close><laserFormNum></laserFormNum><printAllFlag>0</printAllFlag><sepFedStFlag>0</sepFedStFlag><k1LetterCode></k1LetterCode><firmNameFlag>0</firmNameFlag><instrCode></instrCode><instrCodePy></instrCodePy><transLtrReqCode></transLtrReqCode><dba>0</dba><useI>0</useI><dateBlnk>0</dateBlnk><transLetterInGovtCopy>0</transLetterInGovtCopy><prnExtDueDate>0</prnExtDueDate><extPost95></extPost95><year>a</year><instrCode96></instrCode96><coverLetter></coverLetter><filingInstr></filingInstr><dueDateELFFormCode></dueDateELFFormCode><LettersReflectPaperFileWhenEfileDisqualified>0</LettersReflectPaperFileWhenEfileDisqualified><UsePreparedBySectionForCustomFiling>0</UsePreparedBySectionForCustomFiling><IncludeParagraphRefIRSRevenueProcedure2012_17>0</IncludeParagraphRefIRSRevenueProcedure2012_17><SuppressPhoneNumberInK1Letter>0</SuppressPhoneNumberInK1Letter><MovedFromReserved2012></MovedFromReserved2012><DoNotPrepareSeparateLettersForFedForms>0</DoNotPrepareSeparateLettersForFedForms></transmit></officeGroupOptions>";
+                }
+                //find the one section we care about and add/change the new value
+                int index, endindex;
+                string line = "<laserFormNum>";
+                if (output.Contains(line))
+                {
+                    index = output.IndexOf(line);
+                    endindex = output.IndexOf("</", index + 1);
+                    string beginning = output.Substring(0, index);
+                    string ending = output.Substring(endindex);
+                    line = line + FormNum;
+                    output = beginning + line + ending;
+                }
+                using (StreamWriter writer = new StreamWriter(FilePath))
+                {
+                    writer.Write(output);
+                    writer.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+    }
+
+    class Prompt
+    {
+        public static List<int> GetItem(List<string> Groups)
+        {
+            Form prompt = new Form()
+            {
+                Width = 198,
+                Height = 147,
+                FormBorderStyle = FormBorderStyle.FixedToolWindow,
+                StartPosition = FormStartPosition.CenterParent
+            };
+            Label lbApp = new Label()
+            {
+                Left = 10,
+                Top = 14,
+                Text = "Select Office Groups to update :",
+                AutoSize = true
+            };
+            Button btAccept = new Button()
+            {
+                Left = 16,
+                Width = 68,
+                Top = 50 + (Groups.Count * 20),
+                Text = "OK",
+            };
+            Button btCancel = new Button()
+            {
+                Left = 96,
+                Width = 75,
+                Top = 50 + (Groups.Count * 20),
+                Text = "Cancel",
+            };
+            List<CheckBox> chkOfficeGroup = new List<CheckBox>();
+            for (int i = 0; i < Groups.Count; i++)
+            {
+                chkOfficeGroup.Add(new CheckBox()
+                {
+                    Left = 16,
+                    Top = 36 + (i * 20),
+                    Text = Groups[i]
+                });
+                prompt.Controls.Add(chkOfficeGroup[i]);
+            }
+            prompt.Height = 130 + (Groups.Count * 20);
+            btCancel.DialogResult = DialogResult.Cancel;
+            btAccept.DialogResult = DialogResult.OK;
+            prompt.Controls.Add(lbApp);
+            prompt.Controls.Add(btAccept);
+            prompt.Controls.Add(btCancel);
+
+            prompt.CancelButton = btCancel;
+            prompt.AcceptButton = btAccept;
+
+            DialogResult dgResult = prompt.ShowDialog();
+
+            List<int> GroupsChecked = new List<int>();
+
+            if (dgResult == DialogResult.OK)
+            {
+                GroupsChecked.Clear();
+                for(int i = 0; i < chkOfficeGroup.Count; i++)
+                {
+                    if (chkOfficeGroup[i].Checked)
+                        GroupsChecked.Add(i);
+                }
+                if (GroupsChecked.Count == 0)
+                    GroupsChecked.Add(-1);
+                return GroupsChecked;
+            }
+            else
+            {
+                GroupsChecked.Clear();
+                GroupsChecked.Add(-1);
+                return GroupsChecked;
+            }
+        }
+    }
+}
